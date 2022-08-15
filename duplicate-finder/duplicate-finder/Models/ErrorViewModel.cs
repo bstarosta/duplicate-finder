6c6599b4665ac13afc9dfd5506987d4c397510b6
Bartosz Starosta <bstarosta77@gmail.com>
2022-08-15 15:31:28 +0200

    Added project template

---
+++ duplicate-finder/duplicate-finder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace duplicate_finder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
