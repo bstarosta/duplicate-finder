65c55e135e64887caeca8b4965d89e41a0c6cbf8
Bartosz Starosta <bstarosta77@gmail.com>
2022-08-15 17:30:09 +0200

    Added array processing logic

--- duplicate-finder/duplicate-finder/Startup.cs
+++ duplicate-finder/duplicate-finder/Startup.cs
@@ -46,7 +46,7 @@ namespace duplicate_finder
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=InputProcessing}/{action=Index}/{id?}");
             });
         }
     }
